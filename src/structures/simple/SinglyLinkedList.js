6849dce833695ab313b31349e19b771de10c6026
Deivid Alberto Guerra Carpio <dguerrac7@miumg.edu.gt>
2026-04-17 18:24:20 -0600

    Migracion a js reto 4 lista simple y solucion a problemas de ejecucion con los retos anterirores

--- src/structures/simple/SinglyLinkedList.js
+++ src/structures/simple/SinglyLinkedList.js
@@ -65,33 +65,77 @@ countOccurrences(value) {
     return count;
 }
 
-clean() {
-    let removedCount = this._size;
-    while (!this.isEmpty()) {
-        this.removeFirst();
-    }
-    return removedCount;
-}
+  clean() {
+   let removed = 0;              
+    let current = this.head;     
 
-reverseInPlace() {
-    if (this._size <= 1) return;
-    let current = this.head;
-    let temp = null;
     while (current !== null) {
-        temp = current.previous;
-        current.previous = current.next;
-        current.next = temp;
-        current = current.previous;
+      let next = current.next;    
+      current.next = null;       
+      current = next;             
+      removed++;
     }
-    temp = this.head;
-    this.head = this.tail;
-    this.tail = temp;
-}
 
-  removeDuplicates() {
-    throw new Error(
-      "TODO RETO: Implementar removeDuplicates() en SinglyLinkedList."
-    );
+    this.head = null;           
+    this.tail = null;            
+    this._size = 0;              
+    return removed;
+  }
+
+  reverseInPlace() {
+  if (this.head === null || this.head.next === null) { 
+      return;
+    }
+
+    let previous = null;    
+    let current = this.head; 
+    this.tail = this.head;   
+
+    while (current !== null) {
+      let next = current.next;    
+      current.next = previous;    
+      previous = current;        
+      current = next;             
+    }
+
+    this.head = previous; 
+  }
+
+removeDuplicates() {
+    let removed = 0;
+    let current = this.head;
+
+    while (current !== null) {
+      let runnerPrevious = current;
+      let runner = current.next;
+
+      while (runner !== null) {
+        if (this._isSameValue(current.value, runner.value)) {
+          runnerPrevious.next = runner.next;
+
+          if ( runner === this.tail ) {
+            this.tail = runnerPrevious;
+          }
+
+          this.size--;
+          removed++;
+
+          runner = runnerPrevious.next;
+        } else {
+          runnerPrevious = runner;
+          runner = runner.next;
+        }
+      }
+      
+      current = current.next;
+    }
+
+    return removed;
+
+  }
+
+  size() {
+    return this._size;
   }
 
   size() {
